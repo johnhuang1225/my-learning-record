19853122b3c7834b0c310ca647c99a9f7bab5478
Azhen <middletonlee1994@gmail.com>
2016-03-20 17:26:08 +0800

    add global_objects.js

---
+++ global_objects/global_objects.js
@@ -0,0 +1,5 @@
+// absolute path of this file is located
+console.log('This file is located at:        ' + __dirname);
+
+// absolute path of this file
+console.log('The absolute path of this file: ' + __filename);
