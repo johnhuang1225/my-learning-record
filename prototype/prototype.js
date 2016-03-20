5bbe2249b3b67d9e15a93eb995198beecad8a648
Azhen <middletonlee1994@gmail.com>
2016-03-20 16:39:14 +0800

    add prototype.js

---
+++ prototype/prototype.js
@@ -0,0 +1,18 @@
+// Object constructor
+function Person(name) {
+  this.name = name;
+}
+
+// All JavaScript objects inherit their properties and methods from their prototype.
+Person.prototype.sayHello = function(){
+  console.log('Hello my name is ' + this.name)
+}
+
+// Create Person instents by using 'new' keyword
+var jack = new Person('Jack');
+var tom = new Person('Tom');
+
+// this === jack
+jack.sayHello();
+// this === tom
+tom.sayHello();
