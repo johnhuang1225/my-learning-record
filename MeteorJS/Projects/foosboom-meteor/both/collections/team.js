40bc8e569af83a694792bd3096e90b64ba174386
Azhen <middletonlee1994@gmail.com>
2016-03-24 22:00:52 +0800

    add MeteorJS/Projects/foosboom-meteor/

---
+++ MeteorJS/Projects/foosboom-meteor/both/collections/team.js
@@ -0,0 +1,47 @@
+Teams = new Meteor.Collection('teams');
+
+Teams.allow({
+  insert: function (userId, doc) {
+    return (userId && doc.ownerId === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    return doc.ownerId === userId;
+  },
+  remove: function (userId, doc) {
+    return doc.ownerId === userId;
+  },
+  fetch: ['ownerId']
+});
+
+Meteor.methods({
+  teamUpdate: function(teamId, newName){
+    check(Meteor.userId(), String);
+    check(teamId, String);
+    check(newName, String);
+
+    var team = Teams.findOne(teamId);
+    if(team){
+      Teams.update(teamId, {$set: {name: newName}}, function(error){
+        if(!error){
+          if(team.gameIds){
+            var games = Games.find({_id: {$in: team.gameIds}});
+
+            games.fetch().forEach(function(game){
+              game.teams.map(function(team){
+                if(team._id == teamId){
+                  team.name = newName;
+                }
+
+                Games.update({_id: game._id}, {$set: {teams: game.teams}});
+              })
+            });
+          }
+
+          return teamId;
+        }
+      });
+    } else {
+      throw new Meteor.Error("team-does-not-exist", "This team doesn't exist in the database");
+    };
+  }
+});
