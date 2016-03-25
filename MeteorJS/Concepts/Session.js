eea1766c7b39f52b719bb995be31771c28ceb608
Azhen <middletonlee1994@gmail.com>
2016-03-25 10:50:30 +0800

    update MeteorJS/Concepts/

--- MeteorJS/Concepts/Session.js
+++
@@ -1,8 +0,0 @@
-// setDefault() sets a value for a key only if the key is undefined.
-Session.setDefault("key", "default value");
-// Returns default value
-Session.get("key");
-// Assigns a new value to a key
-Session.set("key","new value");
-// Translates to Session.get("key") === "expression" but is more efficient
-Session.equals("key","expression");
