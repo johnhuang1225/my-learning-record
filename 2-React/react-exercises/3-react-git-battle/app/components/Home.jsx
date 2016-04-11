a2180274991820b3b452263850243cdfcd534bc5
Azhen <middletonlee1994@gmail.com>
2016-04-12 07:36:40 +0800

    update Home.jsx

--- 2-React/react-exercises/3-react-git-battle/app/components/Home.jsx
+++ 2-React/react-exercises/3-react-git-battle/app/components/Home.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { Router, Link } from 'react-router'
+import { styles } from '../styles/index.jsx'
 
 export class Home extends Component {
   render() {
     return (
-       <div> Hello from Home! </div>
+      <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Github Battle</h1>
+        <p className='lead'>What even is a jQuery?</p>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+        </Link>
+      </div>
     )
   }
 }
