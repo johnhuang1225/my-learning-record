11f6ef87df10c83a93e36e82f3f3bbc53aab56e8
Azhen <middletonlee1994@gmail.com>
2016-04-11 10:59:01 +0800

    update 4-meteor-react-chat/

--- 1-Meteor/meteor-exercises/4-meteor-react-chat/imports/api/messages/messages.js
+++ 1-Meteor/meteor-exercises/4-meteor-react-chat/imports/api/messages/messages.js
@@ -1,3 +1,10 @@
 import { Mongo } from 'meteor/mongo'
 
 export const Messages = new Mongo.Collection('messages')
+
+// Deny all client-side updates since we will be using methods to manage this collection
+Messages.deny({
+  insert() { return true },
+  update() { return true },
+  remove() { return true }
+})
